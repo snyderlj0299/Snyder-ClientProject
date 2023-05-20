fdfbf4d132a99161f735574e0a2d22eac32ae622
snyderlj0299 <snyderlj0299@uwec.edu>
2023-05-19 21:23:48 -0500

    update

--- scripts.js
+++ scripts.js
@@ -1,22 +1,14 @@
+var coll = document.getElementsByClassName("drink");
+var i;
 
-//Drink Section
-document.addEventListener("DOMContentLoaded", function(){
-	document.querySelector('.drink>.accordion').addEventListener('click', function(){
-		document.querySelector('.drink>article').classList.toggle('show');
-		document.querySelector('.drink span').classList.toggle('rotate');
-	});
-});
-//Food Section
-document.addEventListener("DOMContentLoaded", function(){
-	document.querySelector('.food>.accordion').addEventListener('click', function(){
-		document.querySelector('.food>article').classList.toggle('show');
-		document.querySelector('.food span').classList.toggle('rotate');
-	});
-});
-//Bakery Section
-document.addEventListener("DOMContentLoaded", function(){
-	document.querySelector('.bakery>.accordion').addEventListener('click', function(){
-		document.querySelector('.bakery>article').classList.toggle('show');
-		document.querySelector('.bakery span').classList.toggle('rotate');
-	});
-});
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
